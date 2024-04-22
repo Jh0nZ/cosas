448176dd2bd704e4f106d6eee681b2724105cc65
Jh0nZ <jhondeycraft776@gmail.com>
2024-04-21 21:00:36 -0400

    aa

--- server/settings.py
+++ server/settings.py
@@ -142,6 +142,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR/'static',]
+STATIC_ROOT = BASE_DIR/'staticfiles'
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
