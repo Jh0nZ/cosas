c3b8c43ed884a1926510896b03368e229122de41
Jh0nZ <jhondeycraft776@gmail.com>
2024-04-21 21:19:11 -0400

    aaa

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# exit on error 
-pip install -r requirements.txt
-python3.9 manage.py collectstatic --no-input
-#python manage.py migrate 
--- server/settings.py
+++ server/settings.py
@@ -18,7 +18,9 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR/'static',]
+STATIC_ROOT = BASE_DIR/'staticfiles_build'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -141,9 +143,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR/'static',]
-STATIC_ROOT = BASE_DIR/'staticfiles'
+
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
