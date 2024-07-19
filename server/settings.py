9911dad660e2bd8ff1898b5b52a62f35a68d39f7
Jh0nZ <43425433+Jh0nZ@users.noreply.github.com>
2024-07-19 10:55:38 -0400

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = "server.wsgi.application"
 
 DATABASES = {
     "default": dj_database_url.config(
-        default="postgresql://postgres:postgres@localhost:5432/mysite", conn_max_age=600
+        default="postgresql://jhon:K4OS5I4gYpAj7SDeJBtXFLjPiblCfM5H@localhost:5432/sis", conn_max_age=600
     )
 }
 
